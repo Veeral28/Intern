04bf2f5086895dfbb6b7f6896d5394e091b4e360
Veeral28 <veeralbhatt28@gmail.com>
2024-06-10 15:58:53 +0530

    Add project files.

---
+++ Hello/linear.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Hello
+{
+    public class Linear
+    {
+        public int Linsearch(int[] arr, int n)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == n)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public static void Main(string[] args)
+        {
+            int[] Arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Linear linearSearch = new Linear();
+            int index = linearSearch.Linsearch(Arr, 3);
+            Console.WriteLine("Index of 3: " + index);
+            Console.ReadLine();
+        }
+    }
+}
