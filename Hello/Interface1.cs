602d2bb9b772770ce6a9245269e49d7c92a1f790
Veeral28 <veeralbhatt28@gmail.com>
2024-06-19 16:19:57 +0530

    changes

---
+++ Hello/Interface1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    internal interface Interface1
+    {
+        public void Hello();
+    }
+
+
+    public class Gel: Interface1
+    {
+        public void Hello() 
+        {
+        Console.WriteLine("Hello")
+        }
+}
