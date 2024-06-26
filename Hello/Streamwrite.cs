5033c1be7bfe344b08f446826baa07e6ce3419fd
Veeral28 <veeralbhatt28@gmail.com>
2024-06-26 18:32:24 +0530

    changes

---
+++ Hello/Streamwrite.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    internal class Streamwrite
+    {
+        static void Main(string[] args)
+        {
+            using (StreamWriter sw = new StreamWriter("texts.txt"))
+            {
+                Random r = new Random();
+                for (int i = 1; i <= 5; i++)
+                {
+                    sw.WriteLine(r.Next(500, 6000));
+                }
+            }
+        }
+    }
+}
