a5175e165c1b980340c1483b93690f4d3de62ac9
Veeral28 <veeralbhatt28@gmail.com>
2024-07-01 14:18:28 +0530

    Queue and Stack

---
+++ Hello/Queue.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    internal class Queue
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> q1 = new Queue<int>();
+            q1.Enqueue(1);
+            q1.Enqueue(2);
+            q1.Enqueue(3);
+            q1.Enqueue(4);
+            q1.Enqueue(5);
+            q1.Enqueue(9);
+            int q2 = q1.Count;
+            int q3 = q1.Dequeue();
+            Console.WriteLine(q2.ToString());
+            Console.WriteLine(q2);
+            Console.WriteLine(q3);
+            Console.ReadLine();
+        }
+    }
+}
--- Hello/stack123.cs
+++ Hello/stack123.cs
@@ -1,17 +1,30 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace Hello
-//{
-//    public class stack123
-//    {
-//        Stack<int> stack1 = new Stack<int>();
-//        stack1.Push(1);
-//        stack1.Push(2);
-//        stack1.Push(3);
-//    }
-        
-//}
+namespace Hello
+{
+    public class stack123
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack1 = new Stack<int>();
+            stack1.Push(1);
+            stack1.Push(2);
+            stack1.Push(3);
+            stack1.Push(4);
+            stack1.Push(5);
+
+            int slack =stack1.Pop();
+            int stack2 = stack1.Pop();
+            int stack3 = stack1.Peek();
+            Console.WriteLine(slack);
+            Console.WriteLine(stack2);
+            Console.WriteLine(stack3);
+            Console.ReadLine();
+        }
+    }
+
+}
